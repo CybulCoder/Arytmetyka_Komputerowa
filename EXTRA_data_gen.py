022b7a381cffd312caa42cb1c48910d221ca7440
CybulCoder <kamateo1235@gmail.com>
2026-03-30 02:37:03 +0200

    Add files via upload

--- EXTRA_data_gen.py
+++ EXTRA_data_gen.py
@@ -1,16 +1,15 @@
 from data_gen import Generate_Random_Number
 
-# 9000 limbs for 1 ms
 
 
 filename = 'EXTRA_data.txt'
-for i in range(8500, 9000+1, 100):
+for i in range(8700, 9000+1, 50):
     Generate_Random_Number(i, filename)
     Generate_Random_Number(i, filename)
 
 print("1Done!")
 
-for i in range(3_500_000, 4_000_000+1, 100_000):
+for i in range(3_650_000, 3_700_000+1, 10_000):
     Generate_Random_Number(i, filename)
     Generate_Random_Number(i, filename)
 print("2Done!")
